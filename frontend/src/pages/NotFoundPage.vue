832522cd658521e4eef32c2c89805faae80bd0c1
Jonatas Brito <jonatas.brito.ferreira@gmail.com>
2023-06-01 12:31:07 -0300

    feat: add go home link in not found page

---
+++ frontend/src/pages/NotFoundPage.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div>
+        <h1>NOT FOUND</h1>
+        <router-link to="/home">
+            Home Page
+        </router-link>
+    </div>
+</template>
--- frontend/src/router/routerScript.ts
+++ frontend/src/router/routerScript.ts
@@ -1,7 +1,15 @@
 import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
+import LoginPage from "../pages/LoginPage.vue";
+import NotFoundPage from "../pages/NotFoundPage.vue";
+import RegisterPage from "../pages/RegisterPage.vue";
 
-const routes: RouteRecordRaw[] = [{ path: "/", component: HomePage }];
+const routes: RouteRecordRaw[] = [
+    { path: "/", alias:"/home", component: HomePage },
+    { path: "/login", component: LoginPage },
+    { path: "/register", component: RegisterPage },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFoundPage },
+];
 
 export const router = createRouter({
     routes,
