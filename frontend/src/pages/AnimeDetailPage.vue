2fde3076466aec5219b2a78533f8e12e4cc361d7
Jonatas Brito <jonatas.brito.ferreira@gmail.com>
2023-06-02 16:06:59 -0300

    fix: change anime detail page layout

--- frontend/src/pages/AnimeDetailPage.vue
+++ frontend/src/pages/AnimeDetailPage.vue
@@ -19,19 +19,35 @@ onBeforeMount(async () => {
 <template>
     <div>
         <NavBar />
-        <img
-            :src="useUploadURL(anime.cover.url)"
-            class="card-img-top"
-            alt="anime cover"
-        >
-        <p>{{ anime.title }}</p>
-        <p>{{ anime.rating }}</p>
-        <p>{{ anime.description }}</p>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <img
+                        :src="useUploadURL(anime.cover.url)"
+                        class="card-img-top col-3"
+                        alt="anime cover"
+                    >
+                </div>
+                <div class="col-md-9 col-lg-8">
+                    <h1>{{ anime.title }}</h1>
+                    <p class="text-muted">
+                        Nota: {{ anime.rating }}
+                    </p>
+                    <p class="anime-description">
+                        {{ anime.description }}
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-    img {
-        width: 250px;
-    }
+img {
+    width: 250px;
+}
+
+.anime-description {
+    font-size: 15px;
+}
 </style>
