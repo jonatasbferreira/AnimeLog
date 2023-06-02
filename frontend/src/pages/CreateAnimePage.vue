563a7c775f541dea5360993ce8541c44b7dbe122
Jonatas Brito <jonatas.brito.ferreira@gmail.com>
2023-06-02 17:17:01 -0300

    feat: add create anime page

---
+++ frontend/src/pages/CreateAnimePage.vue
@@ -0,0 +1,80 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div class="container d-flex justify-content-center mt-5">
+        <form>
+            <div class="text-center">
+                <h2 class="fw-normal mb-3 pb-3">
+                    Create Anime
+                </h2>
+            </div>
+
+            <div class="form-outline mb-4">
+                <label
+                    class="form-label"
+                    for="title"
+                >Title</label>
+                <input
+                    id="title"
+                    type="text"
+                    class="form-control"
+                >
+            </div>
+
+            <div class="form-outline mb-4">
+                <label
+                    class="form-label"
+                    for="description"
+                >Description</label>
+                <textarea
+                    id="description"
+                    name="description"
+                    class="form-control"
+                    rows="3"
+                />
+            </div>
+
+            <div class="row gap-3">
+                <div class="col-3 form-outline mb-4">
+                    <label
+                        class="form-label"
+                        for="password"
+                    >Rating</label>
+                    <input
+                        id="password"
+                        type="number"
+                        class="form-control"
+                    >
+                </div>
+
+                <div class="col form-outline mb-4">
+                    <label
+                        class="form-label"
+                        for="cover"
+                    >Cover</label>
+                    <input
+                        id="cover"
+                        type="file"
+                        class="form-control"
+                    >
+                </div>
+            </div>
+
+            <div class="d-grid gap-2">
+                <button
+                    type="submit"
+                    class="btn btn-primary mb-4"
+                >
+                    Add
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+    form {
+        width: 450px;
+    }
+</style>
--- frontend/src/router/routerScript.ts
+++ frontend/src/router/routerScript.ts
@@ -5,6 +5,7 @@ import NotFoundPage from "../pages/NotFoundPage.vue";
 import RegisterPage from "../pages/RegisterPage.vue";
 import AnimeDetailPage from "../pages/AnimeDetailPage.vue";
 import AdminPage from "../pages/AdminPage.vue";
+import CreateAnimePage from "../pages/CreateAnimePage.vue";
 
 const routes: RouteRecordRaw[] = [
     { path: "/", alias:"/home", component: HomePage },
@@ -12,6 +13,7 @@ const routes: RouteRecordRaw[] = [
     { path: "/login", component: LoginPage },
     { path: "/register", component: RegisterPage },
     { path: "/admin", component: AdminPage },
+    { path: "/admin/createanime", component: CreateAnimePage },
     { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFoundPage },
 ];
 
